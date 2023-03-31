36589f512fb877c9a248d80bcc973d5d52b68457
lautaro <lautihernando@gmail.com>
2023-03-30 23:20:30 -0300

    Navbar mockup and project organization

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HelloWordController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HelloWordController.java
@@ -3,7 +3,6 @@ package ar.edu.itba.paw.webapp.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.ViewResolver;
@@ -21,11 +20,8 @@ public class HelloWordController {
     //En el requestMapping podemos ir agregandole cosas para poder tener mas especificaciones
     @RequestMapping( "/")
     public ModelAndView helloWord(){
-
         //El objeto ModelAndView nos deja detener el modelo y la view
-        final ModelAndView mav = new ModelAndView("helloword/index");
-        // Esto es una gran vulnerabilidad ya que el codigo que pongamos ahi lo va a renderear directamente
-        mav.addObject("username","<span style='color:red'>PAW</span>");
+        final ModelAndView mav = new ModelAndView("/views/index");
         return mav;
     }
 
---
+++ webapp/src/main/webapp/WEB-INF/jsp/views/index.jsp
@@ -0,0 +1,9 @@
+<html>
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+    <link href="/css/main.css" rel="stylesheet"/>
+</head>
+<body>
+    <jsp:include page="../components/navBar.jsp"/>
+</body>
+</html>
