f9906d8168a25698b6f5fa4914cf0fbf22b6d83b
Marco4444 <mscilipoti@itba.edu.ar>
2023-04-04 10:13:10 -0300

    added some entities to models module

---
+++ models/src/main/java/models/BorrowRequestImpl.java
@@ -0,0 +1,21 @@
+package models;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+public class BorrowRequestImpl implements BorrowRequest {
+    private final Date dateIssued;
+
+    private final LocalDateTime returnDate;
+
+    private final User userReference;
+
+    private final AssetInstance assetInstance;
+
+    public BorrowRequestImpl(Date dateIssued, LocalDateTime returnDate, User userReference, AssetInstance assetInstance) {
+        this.dateIssued = dateIssued;
+        this.returnDate = returnDate;
+        this.userReference = userReference;
+        this.assetInstance = assetInstance;
+    }
+}
