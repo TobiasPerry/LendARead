126b890e986bfcc89a9b25e09509732e2868fe84
Marco4444 <mscilipoti@itba.edu.ar>
2023-04-04 11:01:02 -0300

    last* commit refactoring contexts

--- models/src/main/java/models/assetLendingContext/implementations/LendingHistoryImpl.java
+++ models/src/main/java/models/assetLendingContext/implementations/LendingHistoryImpl.java
@@ -1,4 +1,12 @@
 package models.assetLendingContext.implementations;
 
+import javax.xml.stream.Location;
+import java.util.List;
+
 public class LendingHistoryImpl {
+    private final List<Location> locationsList;
+
+    public LendingHistoryImpl(List<Location> locationsList) {
+        this.locationsList = locationsList;
+    }
 }
--- models/src/main/java/models/userContext/implementations/LenderBehaviorImpl.java
+++ models/src/main/java/models/userContext/implementations/LenderBehaviorImpl.java
@@ -2,18 +2,19 @@ package models.userContext.implementations;
 
 import models.userContext.interfaces.LenderBehavior;
 import models.assetLendingContext.implementations.LendingHistoryImpl;
+import models.userContext.interfaces.Locations;
 
 import javax.xml.stream.Location;
 import java.util.List;
 
 public class LenderBehaviorImpl implements LenderBehavior {
 
-    private final List<Location> locationsList;
-
     private final LendingHistoryImpl lendingHistory;
 
-    public LenderBehaviorImpl(List<Location> locationsList, LendingHistoryImpl lendingHistory) {
-        this.locationsList = locationsList;
+    private final Locations locations;
+
+    public LenderBehaviorImpl(List<Location> locationsList, LendingHistoryImpl lendingHistory, Locations locations) {
+        this.locations = locations;
         this.lendingHistory = lendingHistory;
     }
 }
