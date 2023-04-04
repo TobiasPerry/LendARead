76a659cbed4f6b1a17c98d4f934a53b97a2ea927
Marco4444 <mscilipoti@itba.edu.ar>
2023-04-04 10:52:23 -0300

    last commit refactoring contexts

--- models/src/main/java/models/assetExistanceContext/interfaces/Asset.java
+++ models/src/main/java/models/assetExistanceContext/interfaces/Asset.java
@@ -1,12 +1,10 @@
 package models.assetExistanceContext.interfaces;
 
-import sun.security.util.ByteArrays;
 
 public interface Asset {
      int getId();
 
      String getName();
-
 
     byte[] getPhoto();
 
