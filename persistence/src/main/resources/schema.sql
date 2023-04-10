9fa8965bf338b02d776701870c246c01d274d0d2
lautaro hernando <lhernando@itba.edu.ar>
2023-04-10 20:10:20 -0300

    create photo table in the resource

--- persistence/src/main/resources/schema.sql
+++ persistence/src/main/resources/schema.sql
@@ -3,9 +3,8 @@ CREATE TABLE IF NOT EXISTS book(
     isbn varchar(100) not null unique,
     author varchar(100) not null,
     title varchar(100) not null,
-    language varchar(100) not null,
-    photo bytea
-    );
+    "language" varchar(100) not null
+);
 CREATE TABLE IF NOT EXISTS users(
     behavior varchar(100),
     id SERIAL primary key,
@@ -19,12 +18,17 @@ CREATE TABLE IF NOT EXISTS location(
     province varchar(100) NOT NULL,
     country varchar(100) NOT NULL,
     address varchar(100)
-    );
+);
+CREATE TABLE IF NOT EXISTS photos(
+    id SERIAL primary key,
+    photo bytea NOT NULL
+);
 CREATE TABLE IF NOT EXISTS AssetInstance(
     id SERIAL primary key,
     assetId INT references book(uid) ON DELETE CASCADE,
     owner INT references users(id) ON DELETE CASCADE,
     locationId INT references location(id) ON DELETE SET NULL,
     physicalCondition varchar(100),
+    photoId INT references photos(id) ON DELETE SET NULL,
     status varchar(100)
 );
