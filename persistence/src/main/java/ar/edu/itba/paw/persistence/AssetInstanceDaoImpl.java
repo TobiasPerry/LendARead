b43e2da42148926ce913707f108d67f3d24c5416
martinippo01 <martinippo@gmail.com>
2023-04-11 20:38:30 -0300

    Getting all assets instances

--- persistence/src/main/java/ar/edu/itba/paw/persistence/AssetInstanceDaoImpl.java
+++ persistence/src/main/java/ar/edu/itba/paw/persistence/AssetInstanceDaoImpl.java
@@ -4,6 +4,8 @@ import ar.edu.itba.paw.models.assetExistanceContext.implementations.AssetInstanc
 import ar.edu.itba.paw.models.assetExistanceContext.implementations.BookImpl;
 import ar.edu.itba.paw.models.assetExistanceContext.implementations.PhysicalCondition;
 import ar.edu.itba.paw.models.assetExistanceContext.interfaces.Book;
+import ar.edu.itba.paw.models.userContext.implementations.LocationImpl;
+import ar.edu.itba.paw.models.userContext.implementations.UserImpl;
 import ar.itba.edu.paw.persistenceinterfaces.AssetInstanceDao;
 import ar.edu.itba.paw.models.assetExistanceContext.interfaces.AssetInstance;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,7 +22,14 @@ public class AssetInstanceDaoImpl implements AssetInstanceDao {
     private final JdbcTemplate jdbcTemplate;
     private static final RowMapper<Integer> ROW_MAPPER_UID = (rs, rownum) -> rs.getInt("uid");
 
-    private static final RowMapper<Book> ROW_MAPPER_BOOK = (rs, rownum) -> new AssetInstanceImpl(rs.getInt("ai.id"), new BookImpl(rs.getString("isbn"), rs.getString("author"), rs.getString("title"), rs.getString("language")), PhysicalCondition.fromString(rs.getString("ai.physicalcondition")), );
+    private static final RowMapper<AssetInstance> ROW_MAPPER_BOOK = (rs, rownum) ->
+            new AssetInstanceImpl(
+                    rs.getInt("ai.id"),
+                    new BookImpl(rs.getString("b.isbn"), rs.getString("b.author"), rs.getString("b.title"), rs.getString("b.language")),
+                    PhysicalCondition.fromString(rs.getString("ai.physicalcondition")),
+                    new UserImpl(rs.getString("u.mail"), "X", "X"),
+                    new LocationImpl(rs.getString("l.zipcode"), rs.getString("l.locality"), rs.getString("l.province"), rs.getString("l.country"))
+                    );
 //new BookImpl(rs.getString("isbn"), rs.getString("author"), rs.getString("title"), rs.getString("language")
     @Autowired
     public AssetInstanceDaoImpl(final DataSource ds) {
@@ -32,16 +41,15 @@ public class AssetInstanceDaoImpl implements AssetInstanceDao {
 
         jdbcTemplate.update(query,assetId,ownerId,locationId,ai.getPhysicalCondition().toString(),"");
 
-
         return Optional.empty();
     }
 
     @Override
     public Optional<List<AssetInstance>> getAllAssetInstances(){
-        String query = "SELECT * FROM ASSETINSTANCE ai JOIN BOOK b ON ai.assetid = b.uid";
+        String query = "select * from assetinstance as ai join book as b on ai.assetid=b.uid join location as l on ai.locationid=l.id join users as u on ai.owner=u.id";
 
-        jdbcTemplate.query(query, ROW_MAPPER_BOOK);
+        List<AssetInstance> assets = jdbcTemplate.query(query, ROW_MAPPER_BOOK);
 
-        return null;
+        return Optional.of(assets);
     }
 }
