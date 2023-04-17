024419b89fa54c361222a595fc9f7c4c02020956
Pedro <pdihax@gmail.com>
2023-04-17 19:40:38 -0300

    Fix Schema to have title as text

--- persistence/src/main/resources/schema.sql
+++ persistence/src/main/resources/schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS book(
     uid SERIAL primary key,
     isbn varchar(100) not null unique,
     author varchar(100) not null,
-    title varchar(100) not null,
+    title text not null,
     "language" varchar(100) not null
 );
 CREATE TABLE IF NOT EXISTS users(
