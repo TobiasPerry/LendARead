4901e83b6f974da0d5f08a76664f23c4c03a3625
lautaro hernando <lhernando@itba.edu.ar>
2023-04-18 08:09:43 -0300

    web forms check the size of the inputs

--- webapp/src/main/java/ar/edu/itba/paw/webapp/form/AddAssetForm.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/form/AddAssetForm.java
@@ -14,14 +14,18 @@ public class AddAssetForm {
     @Isbn
     private String isbn;
     @NotEmpty
+    @Size(min = 3,max = 100)
     private String language;
     @NotEmpty
+    @Size(min = 3,max = 100)
     private String author;
 
     @NotEmpty
+    @Size(min = 3,max = 100)
     private String title;
 
     @NotEmpty
+    @Size(min = 3,max = 100)
     private String physicalCondition;
 
     @NotEmpty
@@ -29,12 +33,15 @@ public class AddAssetForm {
     private String zipcode;
 
     @NotEmpty
+    @Size(min = 3,max = 100)
     private String locality;
 
     @NotEmpty
+    @Size(min = 3,max = 100)
     private String province;
 
     @NotEmpty
+    @Size(min = 3,max = 100)
     private String country;
 
     @NotEmpty
