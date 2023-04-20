f74f77d94b7d4a71220d55ffd925131ff175db65
Marco4444 <mscilipoti@itba.edu.ar>
2023-04-20 16:22:09 -0300

    added register controller and register view

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/RegisterViewController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/RegisterViewController.java
@@ -1,24 +1,23 @@
 package ar.edu.itba.paw.webapp.controller;
 
 import ar.edu.itba.paw.webapp.form.AddAssetForm;
-import ar.edu.itba.paw.webapp.form.LoginForm;
+import ar.edu.itba.paw.webapp.form.RegisterForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
 
 @Controller
-public class LoginViewController {
-    private final String viewName = "views/loginView";
+public class RegisterViewController {
+    private final String viewName = "views/registerView";
 
-    @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public ModelAndView login(@Valid @ModelAttribute final LoginForm loginForm) {
+    @RequestMapping(value = "/register", method = RequestMethod.GET)
+    public ModelAndView login(@Valid @ModelAttribute final RegisterForm registerForm) {
         return new ModelAndView(viewName);
     }
 
 }
+
