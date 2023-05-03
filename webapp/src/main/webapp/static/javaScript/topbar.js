4618dea75a7b0ce191fcb029d822c99606897a02
martinippo01 <martinippo@gmail.com>
2023-05-03 10:53:57 -0300

    Correct an uncaught exception

--- webapp/src/main/webapp/static/javaScript/topbar.js
+++ webapp/src/main/webapp/static/javaScript/topbar.js
@@ -1,5 +1,11 @@
 document.addEventListener("DOMContentLoaded",(event)=>{
     let path = document.querySelector('body').dataset.path;
-    document.querySelector(`#${path}`).classList.add("active");
+    try {
+        const querySelector = document.querySelector(`#${path}`)
+        if (querySelector !== null) {
+            querySelector.classList.add("active");
+        }
+    }catch (error){
 
+    }
 })
