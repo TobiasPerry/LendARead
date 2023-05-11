b0bca94f7e2cee3f800ea4dba9a36611c175a418
martinippo01 <martinippo@gmail.com>
2023-05-11 10:54:21 -0300

    Add i18n for all enums and solved bugs ralated to enums

--- models/src/main/java/ar/edu/itba/paw/models/assetExistanceContext/implementations/PhysicalCondition.java
+++ models/src/main/java/ar/edu/itba/paw/models/assetExistanceContext/implementations/PhysicalCondition.java
@@ -1,19 +1,15 @@
 package ar.edu.itba.paw.models.assetExistanceContext.implementations;
 
 public enum PhysicalCondition {
-    ASNEW("Nuevo"),
-    FINE("Casi Nuevo"),
-    VERYGOOD("Muy Bueno"),
-    GOOD("Bueno"),
-    FAIR("Aceptable"),
-    POOR("Pobre"),
-    EXLIBRARY("Ex-Biblioteca"),
-    BOOKCLUB("Club de Lectura"),
-    BINDINGCOPY("Copia Encuadernada");
-
-    PhysicalCondition(String nameSpanish) {
-        this.nameSpanish = nameSpanish;
-    }
+    ASNEW(),
+    FINE(),
+    VERYGOOD(),
+    GOOD(),
+    FAIR(),
+    POOR(),
+    EXLIBRARY(),
+    BOOKCLUB(),
+    BINDINGCOPY();
 
     public static PhysicalCondition fromString(String value) {
         if (value != null) {
@@ -26,9 +22,4 @@ public enum PhysicalCondition {
         throw new IllegalArgumentException("No enum constant found for value: " + value);
     }
 
-    private final String nameSpanish;
-
-    public String nameSpanish() {
-        return this.nameSpanish;
-    }
 }
