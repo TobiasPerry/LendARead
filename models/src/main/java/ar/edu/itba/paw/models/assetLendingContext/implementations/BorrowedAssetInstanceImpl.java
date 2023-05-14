a91583139b84df576187173a1c72e4660713a8c3
Marco4444 <mscilipoti@itba.edu.ar>
2023-05-14 08:48:06 -0300

    corrected return date only date

--- models/src/main/java/ar/edu/itba/paw/models/assetLendingContext/implementations/BorrowedAssetInstanceImpl.java
+++ models/src/main/java/ar/edu/itba/paw/models/assetLendingContext/implementations/BorrowedAssetInstanceImpl.java
@@ -4,6 +4,9 @@ import ar.edu.itba.paw.models.assetExistanceContext.implementations.AssetInstanc
 import ar.edu.itba.paw.models.assetExistanceContext.interfaces.AssetInstance;
 import ar.edu.itba.paw.models.assetLendingContext.interfaces.BorrowedAssetInstance;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 final public class BorrowedAssetInstanceImpl extends AssetInstanceImpl implements BorrowedAssetInstance {
 
     private final String dueDate;
@@ -13,11 +16,16 @@ final public class BorrowedAssetInstanceImpl extends AssetInstanceImpl implement
 
     public BorrowedAssetInstanceImpl(final AssetInstance assetInstance, final String dueDate, final String borrower, final int lendingId) {
         super(assetInstance.getId(), assetInstance.getBook(), assetInstance.getPhysicalCondition(), assetInstance.getOwner(), assetInstance.getLocation(), assetInstance.getImageId(), assetInstance.getAssetState(),assetInstance.getMaxDays());
-        this.dueDate = dueDate;
+        this.dueDate = formatDate(dueDate);
         this.borrower = borrower;
         this.lendingId = lendingId;
     }
 
+    private static String formatDate(String date) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.S");
+        LocalDateTime dateTime = LocalDateTime.parse(date, formatter);
+        return dateTime.toLocalDate().toString();
+    }
 
 
     @Override
