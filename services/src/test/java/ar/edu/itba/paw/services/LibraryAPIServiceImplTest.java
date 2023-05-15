a91e5a6b0d462189dd9c6713d8bd0b916abde68e
martinippo01 <martinippo@gmail.com>
2023-05-15 10:44:11 -0300

    Add tests for LibraryAPIServiceImplTest

--- services/src/test/java/ar/edu/itba/paw/services/LibraryAPIServiceImplTest.java
+++ services/src/test/java/ar/edu/itba/paw/services/LibraryAPIServiceImplTest.java
@@ -1,18 +1,54 @@
 package ar.edu.itba.paw.services;
 
+import ar.edu.itba.paw.models.assetExistanceContext.interfaces.Book;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import java.io.IOException;
 
 @RunWith(MockitoJUnitRunner.class)
 public class LibraryAPIServiceImplTest {
 
 
-    private static final String ISBN_VALID = "";
+    @InjectMocks
+    private LibraryAPIServiceImpl libraryAPIService;
+
+
+
+    //   The following book is present in the API's library:
+    private static final String BOOK_ISBN_VALID = "9780321125217";
+    private static final String BOOK_TITLE_VALID = "Domain-driven design";
+    private static final String BOOK_AUTHOR_VALID = "Eric Evans";
+    private static final String BOOK_LANGUAGE_VALID = "English";
+
+    //   The following ISBN is not valid:
+    private static final String BOOK_ISBN_INVALID = "9780325217";
 
     @Test
-    public void validISBNTest(){
+    public void validISBNTest() throws IOException {
+        // 1 - Preconditions
 
+        // 2 - Ejercitacion
+        Book book = libraryAPIService.getBookByISBN(BOOK_ISBN_VALID);
+
+        // 3 - Assertions
+        Assert.assertEquals(book.getName(), BOOK_TITLE_VALID);
+        Assert.assertEquals(book.getAuthor(), BOOK_AUTHOR_VALID);
+        Assert.assertEquals(book.getLanguage(), BOOK_LANGUAGE_VALID);
+    }
+
+    @Test(expected = IOException.class)
+    public void invalidISBNTest() throws IOException{
+        // 1 - Preconditions
+
+        // 2 - Ejercitación
+        Book book = libraryAPIService.getBookByISBN(BOOK_ISBN_INVALID);
+
+        // 3 - Assertions
+        Assert.fail();
     }
 
 }
