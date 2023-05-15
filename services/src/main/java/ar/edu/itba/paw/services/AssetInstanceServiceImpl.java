9ebf3c94878f71feedfbf5bd459ab589b793b530
martinippo01 <martinippo@gmail.com>
2023-05-14 21:14:47 -0300

    Remove warnings in AssetInstanceServiceImpl

--- services/src/main/java/ar/edu/itba/paw/services/AssetInstanceServiceImpl.java
+++ services/src/main/java/ar/edu/itba/paw/services/AssetInstanceServiceImpl.java
@@ -2,8 +2,6 @@ package ar.edu.itba.paw.services;
 import ar.edu.itba.paw.exceptions.AssetInstanceNotFoundException;
 import ar.edu.itba.paw.interfaces.AssetInstanceService;
 import ar.edu.itba.paw.models.assetExistanceContext.interfaces.AssetInstance;
-import ar.edu.itba.paw.models.assetExistanceContext.interfaces.Book;
-import ar.edu.itba.paw.models.userContext.interfaces.Location;
 import ar.edu.itba.paw.models.viewsContext.implementations.PageImpl;
 import ar.edu.itba.paw.models.viewsContext.implementations.SearchQueryImpl;
 import ar.edu.itba.paw.models.viewsContext.interfaces.Page;
@@ -15,7 +13,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 
@@ -40,8 +37,7 @@ public class AssetInstanceServiceImpl implements AssetInstanceService {
             throw new AssetInstanceNotFoundException("assetInstance not found");
 
 
-        AssetInstance assetInstance = assetInstanceOpt.get();
-        return assetInstance;
+        return assetInstanceOpt.get();
     }
 
     @Transactional(readOnly = true)
