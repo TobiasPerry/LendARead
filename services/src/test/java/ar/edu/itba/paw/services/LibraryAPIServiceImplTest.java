9b33069d4fadf9d15b9ced50e0f359fa2859d545
martinippo01 <martinippo@gmail.com>
2023-05-15 00:19:49 -0300

    Working on LibraryAPIServiceImplTest

--- services/src/test/java/ar/edu/itba/paw/services/LibraryAPIServiceImplTest.java
+++ services/src/test/java/ar/edu/itba/paw/services/LibraryAPIServiceImplTest.java
@@ -1,4 +1,18 @@
 package ar.edu.itba.paw.services;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
 public class LibraryAPIServiceImplTest {
+
+
+    private static final String ISBN_VALID = "";
+
+    @Test
+    public void validISBNTest(){
+
+    }
+
 }
