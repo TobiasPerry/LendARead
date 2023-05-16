d808a6e3d32bf2a7ddd080d537edc400757b2e7c
martinippo01 <martinippo@gmail.com>
2023-05-16 13:35:48 -0300

    Add a functionality that I can click from assetView and search by that author/language/physicalCondition

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/IndexViewController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/IndexViewController.java
@@ -72,6 +72,10 @@ public class IndexViewController {
         int pageNum = searchFilterSortForm.getCurrentPage();
         final int itemsPerPage = 15;
 
+        System.out.println("phy " + searchFilterSortForm.getPhysicalConditions()); // TODO remove
+        System.out.println("lang " + searchFilterSortForm.getLanguages()); // TODO remove
+        System.out.println("ser " + searchFilterSortForm.getSearch()); // TODO remove
+
         Page page = assetInstanceService.getAllAssetsInstances(
                 pageNum,itemsPerPage,
                 new SearchQueryImpl(
