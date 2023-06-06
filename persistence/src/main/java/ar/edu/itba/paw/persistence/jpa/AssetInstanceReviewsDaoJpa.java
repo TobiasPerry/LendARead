28f192aa3602b83a85f48f11b2bedd981a798ae2
lautaro hernando <lhernando@itba.edu.ar>
2023-06-05 21:25:22 -0300

    add pagination to reviews of assetIsntance

--- persistence/src/main/java/ar/edu/itba/paw/persistence/jpa/AssetInstanceReviewsDaoJpa.java
+++ persistence/src/main/java/ar/edu/itba/paw/persistence/jpa/AssetInstanceReviewsDaoJpa.java
@@ -2,13 +2,14 @@ package ar.edu.itba.paw.persistence.jpa;
 
 import ar.edu.itba.paw.models.assetExistanceContext.implementations.AssetInstanceImpl;
 import ar.edu.itba.paw.models.assetExistanceContext.implementations.AssetInstanceReview;
+import ar.edu.itba.paw.models.viewsContext.implementations.PagingImpl;
 import ar.itba.edu.paw.persistenceinterfaces.AssetInstanceReviewsDao;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.NoResultException;
-import javax.persistence.PersistenceContext;
+import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Repository
 public class AssetInstanceReviewsDaoJpa implements AssetInstanceReviewsDao {
@@ -24,7 +25,7 @@ public class AssetInstanceReviewsDaoJpa implements AssetInstanceReviewsDao {
     @Override
     public double getRating(AssetInstanceImpl assetInstance) {
         try {
-            String hql = "SELECT AVG(r.rating) FROM AssetInstanceReview r WHERE r.assetInstanceId = :assetInstance";
+            String hql = "SELECT AVG(r.rating) FROM AssetInstanceReview as r WHERE r.assetInstance.id = :assetInstance";
             return (Double) em.createQuery(hql)
                     .setParameter("assetInstance", assetInstance.getId())
                     .getSingleResult();
@@ -33,21 +34,35 @@ public class AssetInstanceReviewsDaoJpa implements AssetInstanceReviewsDao {
         }
     }
 
-    @Override
-    public List<AssetInstanceReview> getAssetInstanceReviews(AssetInstanceImpl assetInstance) {
-        return null;
-    }
 
-   /* @Override
+
+    @Override
     @SuppressWarnings("unchecked")
     public PagingImpl<AssetInstanceReview> getAssetInstanceReviews(int pageNum, int itemsPerPage, AssetInstanceImpl assetInstance) {
 
-        final Query queryNative = em.createNativeQuery("SELECT id FROM AssetInstanceReview  WHERE r.assetInstanceId = :assetInstance");
+        final Query queryNative = em.createNativeQuery("SELECT id FROM AssetInstanceReview as r WHERE r.assetInstanceId = :assetInstance ORDER BY r.id DESC LIMIT :limit OFFSET :offset");
 
-        final Query queryCount = em.createNativeQuery("SELECT count(id) FROM AssetInstanceReview  WHERE r.assetInstanceId = :assetInstance");
+        final Query queryCount = em.createNativeQuery("SELECT count(id) FROM AssetInstanceReview as r WHERE r.assetInstanceId = :assetInstance");
 
+        final int offset = (pageNum - 1) * itemsPerPage;
 
-        String hql = "SELECT r FROM AssetInstanceReview r WHERE r.assetInstanceId = :assetInstance";
-        return (List<AssetInstanceReview>) em.createQuery(hql).setParameter("assetInstance", assetInstance.getId()).getResultList();
-    }*/
+        queryCount.setParameter("assetInstance",assetInstance.getId());
+        queryNative.setParameter("limit",itemsPerPage);
+        queryNative.setParameter("offset",offset);
+        queryNative.setParameter("assetInstance",assetInstance.getId());
+
+        final int totalPages = (int) Math.ceil((double) ((Number) queryCount.getSingleResult()).longValue() / itemsPerPage);
+
+        @SuppressWarnings("unchecked")
+        List<Long> list = (List<Long>) queryNative.getResultList().stream().map(
+                n -> (Long) ((Number) n).longValue()).collect(Collectors.toList());
+        if(list.isEmpty())
+            return new PagingImpl<>(new ArrayList<>(), pageNum, totalPages);
+        final TypedQuery<AssetInstanceReview> query = em.createQuery("FROM AssetInstanceReview AS ai WHERE id IN (:ids) ORDER BY ai.id DESC" , AssetInstanceReview.class);
+        query.setParameter("ids", list);
+        List<AssetInstanceReview> reviewList = query.getResultList();
+
+        return new PagingImpl<>(reviewList, pageNum, totalPages);
+    }
+
 }
