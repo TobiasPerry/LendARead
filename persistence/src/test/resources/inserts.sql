7f14c0b474e035431694e104c33a53be02e4fcdf
lautaro hernando <lhernando@itba.edu.ar>
2023-06-25 13:56:19 -0300

    add review testing

--- persistence/src/test/resources/inserts.sql
+++ persistence/src/test/resources/inserts.sql
@@ -9,3 +9,5 @@ INSERT INTO photos(id,photo) VALUES (0,null);
 INSERT INTO assetinstance(id,assetid, owner, locationid, physicalcondition, photoid, status,ISRESERVABLE,MAXLENDINGDAYS) VALUES (0,0,0,0,'ASNEW',0,'PUBLIC',true,7);
 
 INSERT INTO resetpasswordinfo(id,token,userid,expiration) VALUES (0,'TOKEN',1, TIMESTAMP '2010-07-16 18:19:00-8:00');
+
+INSERT INTO lendings(id,assetinstanceid, borrowerid, lenddate, devolutiondate, active) VALUES (0,0,0, TIMESTAMP '2010-07-16 18:19:00-8:00', TIMESTAMP '2010-07-16 18:19:00-8:00','ACTIVE');
