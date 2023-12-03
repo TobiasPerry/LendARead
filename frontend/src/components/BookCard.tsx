4fe9b62c811cd687f466092e3145da792881fd0c
martinippo01 <84152110+martinippo01@users.noreply.github.com>
2023-12-03 17:32:54 -0300

    Add Bootstrap (not react bootstrap) and bookcard

--- frontend/src/components/BookCard.tsx
+++ frontend/src/components/BookCard.tsx
@@ -1,25 +1,51 @@
-import React from 'react';
 
-function BookCard({ showSearchbar }) {
-    return (
-        <nav className="navbar navbar-expand-lg" style={{ backgroundColor: '#111711' }} data-bs-theme="dark">
-            
-            { (
-                <div className="form mx-3" style={{ marginBlockEnd: 0 }}>
-                    <i className="fa fa-search fa-search-class"></i>
-                    <input
-                        type="text"
-                        className="form-input"
-                        name="search"
-                        style={{ marginLeft: '4px' }}
-                        placeholder="Search..."
-                        id="nav-bar-search-bar"
-                    />
-                </div>)
-            }
-            {/* ... Your NavBar code */}
-        </nav>
-    );
-}
+import '../styles/neoBookCard.css';
+
+const BookCard = () => {
+  return (
+    <>
+      <div className="card text-white card-has-bg click-col cardBook" 
+        style=
+        {{  
+          backgroundImage: "url('http://pawserver.it.itba.edu.ar/paw-2023a-03/getImage/71')",
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+          height: '400px',
+          margin: '15px',
+          width: '18rem',
+          objectFit: 'cover',
+        }}
+          >
+
+    <a href="/" id="infoRef" style={{textDecoration: 'none'}} >
+
+        <img className="card-img d-none" src="http://pawserver.it.itba.edu.ar/paw-2023a-03/getImage/61" alt="Book title" />
+
+        <div className="card-img-overlay d-flex flex-column">
+            <div className="card-body">
+                <small className="card-meta mb-2 text-truncate">Phill Knight</small>
+                <h3 className="card-title mt-0 text-white truncate-3-lines">Shoe Dog</h3>
+                <small className="text-white"><i className="bi bi-book-half text-white"></i> Physical condition </small>
+            </div>
+            <div className="card-footer">
+                <div className="media">
+                    <img className="mr-3 rounded-circle" src="http://pawserver.it.itba.edu.ar/paw-2023a-03/getImage/61  " style={{width:'50px', height: '50px'}}/>
+                    <div className="media-body">
+                        <h6 className="my-0 text-white d-block text-truncate">Usuario</h6>
+                        <small className="text-white truncate-3-lines"> Localidad, Provincia, Pais </small>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </a>
+
+</div>
+    </>
+  );
+
+  
+};
 
 export default BookCard;
--- frontend/src/views/Discovery.tsx
+++ frontend/src/views/Discovery.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-
-
+import Grid from '@mui/material/Grid';
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/system/Stack';
+import { styled } from '@mui/system';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Filters from '../components/Filters';
+import BookCard from '../components/BookCard';
 
 const bull = (
     <Box
@@ -19,9 +26,15 @@ const searchBarStyle: React.CSSProperties = {
 
 }
 
+
 const DiscoveryView = () => {
 
-
+    const Item = styled('div')(({ theme }) => ({
+        // backgroundColor: theme.palette.mode === 'dark' ? '#262B32' : '#fff',
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        borderRadius: 15,
+      }));
 
     return (
         <>
@@ -32,7 +45,7 @@ const DiscoveryView = () => {
                     justifyContent: 'center',
                     flexDirection: 'column', // Optional: Align vertically as well
                     alignItems: 'center', // Optional: Align vertically as well
-                    '& > :not(style)': { m: 5, width: '50%' },
+                    '& > :not(style)': { marginTop: 4, marginBottom: 2, width: '50%' },
                 }}
                 noValidate
                 autoComplete="off"
@@ -45,6 +58,22 @@ const DiscoveryView = () => {
                     variant="outlined" 
                 />
             </Box>  
+
+            <Grid container spacing={2}  sx={{padding: 2}}>
+                <Grid item xs={2}>
+                    <Filters></Filters>
+                </Grid>
+                <Grid item xs={10}>
+                    <Box className='bg-light-green' sx={{ width: '100%' , height: '100%'}}>
+                        <br></br>
+                        <BookCard></BookCard>
+                    </Box>
+                </Grid>                    
+            </Grid>
+            <Box className='p-5' sx={{ width: '100%', display: 'flex', justifyContent: 'center'}}>
+                <Pagination count={10} color="primary" />
+            </Box>
+
         </>
     );
 };
