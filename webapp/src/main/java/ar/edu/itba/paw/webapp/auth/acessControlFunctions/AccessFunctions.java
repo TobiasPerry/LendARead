e822f37983c23fc83aae8af92c6038d6fd04f8f6
lautaro hernando <lhernando@itba.edu.ar>
2023-12-14 17:56:38 -0300

    catch exception to accessFunctions

--- webapp/src/main/java/ar/edu/itba/paw/webapp/auth/acessControlFunctions/AccessFunctions.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/auth/acessControlFunctions/AccessFunctions.java
@@ -32,18 +32,34 @@ public class AccessFunctions {
 
 
 
+    public boolean checkUser(HttpServletRequest request, int id) {
+        try {
+            return userService.getUser(userService.getCurrentUser()).getId() == id;
+        }catch (UserNotFoundException e){
+            return false;
+        }
+    }
+    public boolean locationOwner(HttpServletRequest request, Integer id) {
+        try {
+            return locationsService.getLocation(id).getUser().getEmail().equals(userService.getCurrentUser());
+        }catch (LocationNotFoundException e){
+            return false;
+        }
+    }
 
-    public boolean checkUser(HttpServletRequest request, int id) throws UserNotFoundException {
-        return userService.getUser(userService.getCurrentUser()).getId() == id;
+    public boolean lendingOwner(HttpServletRequest request, Integer id)  {
+        try {
+            return assetAvailabilityService.getLender(id).getEmail().equals(userService.getCurrentUser());
+        }catch (AssetInstanceNotFoundException e){
+            return false;
+        }
     }
-    public boolean locationOwner(HttpServletRequest request, Integer id) throws LocationNotFoundException {
-        return locationsService.getLocation(id).getUser().getEmail().equals(userService.getCurrentUser());
-    }
-    public boolean lendingOwner(HttpServletRequest request, Integer id) throws AssetInstanceNotFoundException {
-        return assetAvailabilityService.getLender(id).getEmail().equals(userService.getCurrentUser());
-    }
-    public boolean assetInstanceOwner(HttpServletRequest request, Integer id) throws AssetInstanceNotFoundException {
-        return assetInstanceService.isOwner(id, userService.getCurrentUser());
+    public boolean assetInstanceOwner(HttpServletRequest request, Integer id){
+        try {
+            return assetInstanceService.isOwner(id, userService.getCurrentUser());
+        }catch (AssetInstanceNotFoundException e){
+            return false;
+        }
     }
 
 
