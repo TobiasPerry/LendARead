aa8dc47ab5e719228a9a7ab6d0c5af9a977fa8c4
Marco4444 <mscilipoti@itba.edu.ar>
2024-01-03 16:41:49 -0300

    login hook working

--- frontend/src/hooks/users/useLogin.ts
+++ frontend/src/hooks/users/useLogin.ts
@@ -4,12 +4,6 @@ import {Api} from "../api/api.ts";
 const useLogin = () => {
     const login = async (email: string, password: string, rememberMe: boolean = false, path: string = "/assets") => {
         try {
-            const responseBare: any = await fetch("http://127.0.0.1:8082/api" + path, {
-                method: "GET",
-                headers: {
-                    "Authorization": "Basic " + btoa(`${email}:${password}`)
-                }
-            });
 
             const response: any = await Api.get(path,
                 {
@@ -19,16 +13,7 @@ const useLogin = () => {
             );
 
 
-            console.log('response', response);
-
-            const jwt = response.headers.get('jwt');
-            console.log('jwt Api', jwt);
-
-            const jwt2 = responseBare.headers.get('jwt');
-            console.log('jwt Barebones', jwt2);
-
-
-            return true;
+            return response.headers.has('jwt')
         } catch (error) {
             console.log("error raised", error);
             return false;
