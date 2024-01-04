c33b9c95097f90957be11d69e10a58b092e684f8
Marco4444 <mscilipoti@itba.edu.ar>
2024-01-04 14:25:01 -0300

    added pagination

--- frontend/src/components/user/MyBooksTable.tsx
+++ frontend/src/components/user/MyBooksTable.tsx
@@ -1,22 +1,20 @@
-import  { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import useAssetInstances from '../../hooks/AssetInstance/useUserAssetInstances.ts'; // Path to your custom hook
 
 const MyBooksTable = () => {
     const { t } = useTranslation();
-    const [books, setBooks] = useState([]); // This will be your books data
-    const { setFilter, filter, applyFilterAndSort, sort, setSort } = useAssetInstances(setBooks);
+    const { setFilter, filter, applyFilterAndSort, sort, setSort, currentPage, changePage, totalPages, books,setBooks } = useAssetInstances();
 
     const handleFilterChange = (newFilter: string) => {
         setFilter(newFilter);
-        applyFilterAndSort(sort, newFilter, books);
+        applyFilterAndSort(currentPage, sort, newFilter, books);
     };
 
     const handleSortChange = (column: string) => {
         const isAsc = sort.column === column && sort.order === 'asc';
         const newSort = { column, order: isAsc ? 'desc' : 'asc' }
         setSort(newSort);
-        applyFilterAndSort(newSort, filter, books);
+        applyFilterAndSort(currentPage, newSort, filter, books);
     };
 
     const renderSortIcon = (column: string) => {
@@ -25,14 +23,16 @@ const MyBooksTable = () => {
 
     return (
         <div className="container mt-3">
-            <h2>{t('borrowed_books')}</h2>
-            <div className="btn-group mb-3" role="group">
+            <div className="d-flex justify-content-between align-items-center">
+                <h2>{t('borrowed_books')}</h2>
+                <div className="btn-group">
                 <button onClick={() => handleFilterChange('all')} className="btn btn-outline-primary">{t('all')}</button>
                 <button onClick={() => handleFilterChange('pending')} className="btn btn-outline-primary">{t('pending')}</button>
                 <button onClick={() => handleFilterChange('delivered')} className="btn btn-outline-primary">{t('delivered')}</button>
                 <button onClick={() => handleFilterChange('overdue')} className="btn btn-outline-primary">{t('overdue')}</button>
                 <button onClick={() => handleFilterChange('rejected')} className="btn btn-outline-primary">{t('rejected')}</button>
                 <button onClick={() => handleFilterChange('finished')} className="btn btn-outline-primary">{t('finished')}</button>
+            </div>
             </div>
             <table className="table table-hover">
                 <thead className="table-light">
@@ -62,13 +62,23 @@ const MyBooksTable = () => {
                 )}
                 </tbody>
             </table>
-            <nav aria-label="Page navigation example">
-                <ul className="pagination">
-                    <li className="page-item"><a className="page-link" href="#">{t('previous')}</a></li>
-                    {/* Add page numbers here */}
-                    <li className="page-item"><a className="page-link" href="#">{t('next')}</a></li>
-                </ul>
-            </nav>
+                <nav aria-label="Page navigation example" className="d-flex justify-content-center">
+                    <ul className="pagination">
+                        {currentPage > 1 && (
+                            <li className="page-item">
+                                <a className="page-link" onClick={() => changePage(currentPage - 1)}>{t('previous')}</a>
+                            </li>
+                        )}
+                        <li className="page-item">
+                            <span className="page-link">{`${currentPage} / ${totalPages}`}</span>
+                        </li>
+                        {currentPage < totalPages && (
+                            <li className="page-item">
+                                <a className="page-link" onClick={() => changePage(currentPage + 1)}>{t('next')}</a>
+                            </li>
+                        )}
+                    </ul>
+                </nav>
         </div>
     );
 };
--- frontend/src/hooks/AssetInstance/useUserAssetInstances.ts
+++ frontend/src/hooks/AssetInstance/useUserAssetInstances.ts
@@ -1,15 +1,27 @@
 // useAssetInstances.js
 import { useState } from 'react';
 
-const useUserAssetInstances = (setBooks: any, initialSort = { column: 'title', order: 'asc' }) => {
+const useUserAssetInstances = (initialSort = { column: 'title', order: 'asc' }) => {
     const [filter, setFilter] = useState('all');
     const [sort, setSort] = useState(initialSort);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [itemsPerPage, setItemsPerPage] = useState(10); // Or whatever default you prefer
+    const [totalPages, setTotalPages] = useState(20); // To be set when data is fetched
+    const [books, setBooks] = useState([])
 
-    const applyFilterAndSort = (newSort: any, newFilter: string, books: any) => {
-        console.log(newSort, newFilter)
+    const applyFilterAndSort = (newPage: number, newSort: any, newFilter: string, books: any) => {
+        console.log(newSort, newFilter, newPage)
     };
 
-    return { setFilter, filter, applyFilterAndSort, sort, setSort };
+    // Function to change the page
+    const changePage = (newPage: number) => {
+        setCurrentPage(newPage);
+        // Re-apply filter and sort whenever page changes
+        applyFilterAndSort(newPage, sort, filter, books);
+    };
+
+
+    return { setFilter, filter, applyFilterAndSort, sort, setSort, currentPage, changePage, totalPages, books, setBooks};
 };
 
 export default useUserAssetInstances;
