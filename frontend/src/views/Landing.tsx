dd942f5e089e4ec821db7e404508737eda09ce97
martinippo01 <martinippo@gmail.com>
2024-01-04 17:15:14 -0300

    i18n for the landing

--- frontend/src/views/Landing.tsx
+++ frontend/src/views/Landing.tsx
@@ -1,20 +1,23 @@
 import BookCard from "../components/BookCard.tsx";
 import './styles/landing.css';
 import {Link} from "react-router-dom";
+import {useTranslation} from "react-i18next";
 
 
 export default function Landing(){
+
+    const {t} = useTranslation();
 
     return (
         <>
             <section id="hero" style={{backgroundColor: '#D0DCD0', paddingBottom: '100px',}}>
                 <div className="container" style={{textAlign: 'start'}}>
                     <div className="text-container">
-                        <h1>Title</h1>
-                        <h2>subtitle</h2>
+                        <h1>{t('landing.hero.title')}</h1>
+                        <h2>{t('landing.hero.subtitle')}</h2>
 
                         <div className="d-flex justify-content-center mt-4">
-                            <Link to="/discovery" className="btn-get-started scrollto" style={{textDecoration: 'none'}}>text</Link>
+                            <Link to="/discovery" className="btn-get-started scrollto" style={{textDecoration: 'none'}}>{t('landing.hero.btn')}</Link>
                         </div>
                     </div>
                     <div className="image-container">
@@ -26,7 +29,7 @@ export default function Landing(){
             
             <section style={{backgroundColor: '#FFFFFF', marginTop: '100px'}}>
                 <div className="container-row-wrapped">
-                    <h1>text</h1>
+                    <h1>{t('landing.recent.title')}</h1>
                 </div>
                 <div className="container-row-wrapped" style={{margin: '20px auto', paddingTop: '20px', backgroundColor: '#FFFFFF', borderRadius: '20px', width: '90%'}}>
                     <BookCard></BookCard>                    
@@ -35,32 +38,32 @@ export default function Landing(){
             
 
             <section className="content-section text-white text-center pb-5" id="services" style={{backgroundColor: '#D0DCD0'}}>
-                <div className="container px-4 px-lg-5">
+                <div className="container px-4 px-lg-5 pt-3">
                     <div className="content-section-heading">
-                        <h2 className="my-5" style={{color: '#3e4450'}}>Titulo</h2>
+                        <h2 className="my-5" style={{color: '#3e4450'}}>{t('landing.instructions.title')}</h2>
                     </div>
                     <div className="row gx-4 gx-lg-5" style={{display: 'flex', justifyContent: 'space-between'}}>
                         <div className="service-item" style={{flex: '0 0 25%', maxWidth: '25%'}}>
                             <span className="service-icon rounded-circle mx-auto mb-3"><i className="bi bi-search"></i></span>
-                            <h4 style={{color: '#2B3B2B'}}><strong>tect</strong></h4>
-                            <p className="mb-0" style={{color: '#3e4450'}}>text</p>
+                            <h4 style={{color: '#2B3B2B'}}><strong>{t('landing.instructions.search.title')}</strong></h4>
+                            <p className="mb-0" style={{color: '#3e4450'}}>{t('landing.instructions.search.subtitle')}</p>
                         </div>
                         <div className="service-item" style={{flex: '0 0 25%', maxWidth: '25%'}}>
                             <span className="service-icon rounded-circle mx-auto mb-3"><i className="bi bi-send"></i></span>
-                            <h4 style={{color: '#2B3B2B'}}><strong>text</strong></h4>
-                            <p className="mb-0" style={{color: '#3e4450'}}>text</p>
+                            <h4 style={{color: '#2B3B2B'}}><strong>{t('landing.instructions.request.title')}</strong></h4>
+                            <p className="mb-0" style={{color: '#3e4450'}}>{t('landing.instructions.request.subtitle')}</p>
                         </div>
                         <div className="service-item" style={{flex: '0 0 25%', maxWidth: '25%'}}>
                             <span className="service-icon rounded-circle mx-auto mb-3"><i className="bi bi-person-circle"></i></span>
-                            <h4 style={{color: '#2B3B2B'}}><strong>text</strong></h4>
+                            <h4 style={{color: '#2B3B2B'}}><strong>{t('landing.instructions.contact.title')}</strong></h4>
                             <p className="mb-0" style={{color: '#3e4450'}}>
-                                text
+                                {t('landing.instructions.contact.subtitle')}
                             </p>
                         </div>
                         <div className="service-item" style={{flex: '0 0 25%', maxWidth: '25%'}}>
                             <span className="service-icon rounded-circle mx-auto mb-3"><i className="bi bi-arrow-left-right"></i></span>
-                            <h4 style={{color: '#2B3B2B'}}><strong>text</strong></h4>
-                            <p className="mb-0" style={{color: '#3e4450'}}>text</p>
+                            <h4 style={{color: '#2B3B2B'}}><strong>{t('landing.instructions.meet.title')}</strong></h4>
+                            <p className="mb-0" style={{color: '#3e4450'}}>{t('landing.instructions.meet.subtitle')}</p>
                         </div>
                     </div>
 
