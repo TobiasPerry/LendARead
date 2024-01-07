e10c8edf162583053eb2f07b60ce8aa931afedaa
lautaro hernando <lhernando@itba.edu.ar>
2024-01-07 18:02:49 -0300

    fix Bearer token

--- webapp/src/main/java/ar/edu/itba/paw/webapp/auth/filters/JwtTokenFilter.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/auth/filters/JwtTokenFilter.java
@@ -42,8 +42,9 @@ public class JwtTokenFilter extends OncePerRequestFilter {
             chain.doFilter(request, response);
             return;
         }
-        final String token = header.split(" ")[1].trim();
+        final String token;
         try {
+            token = header.split(" ")[1].trim();
             if (!jwtTokenUtil.validateJwtToken(token)) {
                 chain.doFilter(request, response);
                 return;
