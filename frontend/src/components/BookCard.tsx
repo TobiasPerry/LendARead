c109ec9ebbdce9bb1a7f94eccc59c00cc8286b53
martinippo01 <martinippo@gmail.com>
2024-01-09 11:52:58 -0300

    Minor changes

--- frontend/src/components/BookCard.tsx
+++ frontend/src/components/BookCard.tsx
@@ -6,7 +6,6 @@ const BookCard = ({ book }) => {
     const {
         title,
         author,
-        language,
         image,
         physicalCondition,
         userImage,
@@ -48,7 +47,7 @@ const BookCard = ({ book }) => {
             </div>
             <div className="card-footer">
                 <div className="media">
-                    <img className="mr-3 rounded-circle" src={userImage} style={{width:'50px', height: '50px'}}/>
+                    <img className="mr-3 rounded-circle" src={userImage} style={{width:'50px', height: '50px'}} alt="User Image"/>
                     <div className="media-body">
                         <h6 className="my-0 text-white d-block text-truncate">{userName}</h6>
                         <small className="text-white truncate-3-lines"> {locality}, {province}, {country} </small>
