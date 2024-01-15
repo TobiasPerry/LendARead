b783aded84a32b96ba9df4988fe7a22870d2561b
Marco4444 <mscilipoti@itba.edu.ar>
2024-01-15 18:19:59 -0300

    stlying of modal pretty good

--- frontend/src/components/modals/ChangeStatusModal.tsx
+++ frontend/src/components/modals/ChangeStatusModal.tsx
@@ -8,8 +8,11 @@ function ChangeStatusModal({ asset, showModal, handleCloseModal, handleSubmitMod
     return (
         <>
             <div className={`modal ${showModal ? 'show' : ''}`}  role="dialog" aria-labelledby="modalTitle">
-                <div className="modal-dialog" role="document">
-                    <div className="modal-content">
+
+                    <div className="modal-dialog modal-content" style={{
+                        backgroundColor: '#f0f5f0',
+                        borderRadius: '20px',
+                    }}>
                         <div className="modal-header">
                             <div className="icon-box">
                                 <i className="fas fa-eye-slash fa-lg"></i>
@@ -17,21 +20,20 @@ function ChangeStatusModal({ asset, showModal, handleCloseModal, handleSubmitMod
                             <h2 className="modal-title" id="modalTitle">
                                 {t('userHomeView.changeVisibilityTitle')}
                             </h2>
+
+                            <button onClick={handleCloseModal} className="btn btn-secondary">
+                                <i className="fas fa-window-close fa-lg"></i>
+                            </button>
                         </div>
                         <div className="modal-body">
                             <p>
                                 {!isPrivate(asset.state) ? t('userHomeView.changeVisibilityTextPublic') : t('userHomeView.changeVisibilityTextPrivate')}
                             </p>
                         </div>
-                        <div className="modal-footer">
-                            <button type="submit" className="btn btn-primary" onClick={handleSubmitModal}>
+                            <button type="submit" className="btn btn-green" onClick={handleSubmitModal}>
                                 {isPrivate(asset.state) ? t('userHomeView.makePrivate') : t('userHomeView.makePublic')}
                             </button>
-                            <button onClick={handleCloseModal} className="btn btn-secondary">
-                                Close
-                            </button>
-                        </div>
-                    </div>
+
                 </div>
             </div>
         </>
