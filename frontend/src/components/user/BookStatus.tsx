5ffc2237d5c2725820907a07da53c6a197a7d4f6
Marco4444 <mscilipoti@itba.edu.ar>
2024-01-20 15:03:50 -0300

    added status book stylying

--- frontend/src/components/user/BookStatus.tsx
+++ frontend/src/components/user/BookStatus.tsx
@@ -8,17 +8,22 @@ const BookStatus = ({asset}) => {
     const {getLocation, location} = useLocationAsset()
     useEffect(() => { getLocation(asset).then()}, [asset])
 
+    const styles = {
+        backgroundColor: '#f0f5f0',
+        borderRadius: '20px',
+        padding: "20px"
+    }
     return (
         <>
         {/*Lended/Borrowed asset instance*/}
         {!(asset === undefined || asset.lending === undefined) &&
-        <div className="loan-status card p-5">
-            <h5 className="card-title">{t('status')}:{asset.lending.state}</h5>
+        <div style={styles}>
+            <h3 className="card-title">{t('statusLabel')}:{asset.lending.state}</h3>
         </div> }
             {/* User asset instance*/}
             {(asset !== undefined && asset.lending === undefined) &&
-                <div className="loan-status card p-5">
-                    <h3>{t("status")}</h3>
+                <div style={styles}>
+                    <h3>{t("statusLabel")}</h3>
                     <h5 className="card-title">{t('maxDays')}:{asset.maxDays}</h5>
                     <h5>{t("location")} : {location.country}, {location.province}, {location.locality}, {location.zipcode}</h5>
                 </div>
