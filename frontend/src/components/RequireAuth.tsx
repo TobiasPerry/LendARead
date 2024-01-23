f057cf083354c462c123830072d3b3eaa8f8f57b
Marco4444 <mscilipoti@itba.edu.ar>
2024-01-23 14:13:03 -0300

    added authorized routes

---
+++ frontend/src/components/RequireAuth.tsx
@@ -0,0 +1,14 @@
+import LoginView from "../views/user/LogIn.tsx";
+import {useContext} from "react";
+import {AuthContext} from "../contexts/authContext.tsx";
+
+const RequireAuth = ({ children }) => {
+   const {isLoggedIn} = useContext(AuthContext);
+
+   if (!isLoggedIn)
+      return <LoginView />;
+
+   return children;
+};
+
+export default RequireAuth;
