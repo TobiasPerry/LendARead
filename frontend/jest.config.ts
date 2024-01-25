c38b641181ba043ff2c00a9ead7ade1c369c0328
Marco4444 <mscilipoti@itba.edu.ar>
2024-01-25 19:54:33 -0300

    trying to add AuthContextProvider but not successfull, changing approach

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -8,8 +8,8 @@ const config: Config.InitialOptions = {
     moduleNameMapper: {
         // Handle module aliases (if you are using them in your project)
         '^@/(.*)$': '<rootDir>/src/$1',
-        '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
-        '\\.(css|scss|sass)$': '<rootDir>/__mocks__/styleMock.js',
+        '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/src/__mocks__/fileMock.js',
+        '\\.(css|scss|sass)$': '<rootDir>/src/__mocks__/styleMock.js',
     },
     transform: {
         "^.+\\.(ts|tsx)$": "babel-jest", // Transform TypeScript files
--- frontend/src/__tests__/Landing.tsx
+++ frontend/src/__tests__/Landing.tsx
@@ -6,6 +6,53 @@ import { I18nextProvider } from 'react-i18next';
 import i18n from '../i18n.js'
 import AuthContextProvider, {AuthContext} from "../contexts/authContext.tsx";
 
+// Landing.test.tsx
+
+// Mock localStorage and sessionStorage
+const localStorageMock = (function () {
+    let store = {};
+    return {
+        getItem(key) {
+            return store[key] || null;
+        },
+        setItem(key, value) {
+            store[key] = value.toString();
+        },
+        clear() {
+            store = {};
+        },
+        removeItem(key) {
+            delete store[key];
+        },
+    };
+})();
+
+const sessionStorageMock = (function () {
+    let store = {};
+    return {
+        getItem(key) {
+            return store[key] || null;
+        },
+        setItem(key, value) {
+            store[key] = value.toString();
+        },
+        clear() {
+            store = {};
+        },
+        removeItem(key) {
+            delete store[key];
+        },
+    };
+})();
+
+Object.defineProperty(window, 'localStorage', {
+    value: localStorageMock,
+});
+
+Object.defineProperty(window, 'sessionStorage', {
+    value: sessionStorageMock,
+});
+
 
 describe('Landing Component', () => {
     test('renders without crashing', () => {
@@ -20,8 +67,11 @@ describe('Landing Component', () => {
 
     test('displays the get started button', () => {
         render(
+            <AuthContextProvider>
             <Landing />
-       );
+            </AuthContextProvider>
+
+        );
         const buttonElement = screen.getByText(/landing.hero.btn/i);
         expect(buttonElement).toBeInTheDocument();
     });
