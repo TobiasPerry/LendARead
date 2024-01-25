4ab5f402510e1a2fa0194df6217cee4cdefe3f74
Marco4444 <mscilipoti@itba.edu.ar>
2024-01-25 20:07:07 -0300

    left the Lending test, just need to manage correct mocking to make it work

--- frontend/src/__tests__/Dummy.tsx
+++ frontend/src/__tests__/Dummy.tsx
@@ -1,13 +1,9 @@
-// Landing.test.tsx
 import { render, screen } from '@testing-library/react';
-import Landing from '../views/Landing.tsx';
 import React from 'react'; // Add this line
-import { I18nextProvider } from 'react-i18next';
-import i18n from '../i18n.js'
-import AuthContextProvider, {AuthContext} from "../contexts/authContext.tsx";
-import App from "../App.tsx";
 import Dummy from "../views/dummyView.tsx";
+import {setupTests} from "../testUtils/setupTests.js.ts";
 
+setupTests()
 describe('Landing Component', () => {
     test('renders without crashing', () => {
         render(
@@ -16,5 +12,4 @@ describe('Landing Component', () => {
         const titleElement = screen.getByText("does this pass tests??");
         expect(titleElement).toBeInTheDocument();
     });
-
 });
---
+++ frontend/src/__tests__/Landing.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import AuthContextProvider from "../contexts/authContext.tsx";
+import { render, screen } from '@testing-library/react';
+import Landing from "../views/Landing.tsx";
+import {setupTests} from "../testUtils/setupTests.js.ts";
+
+
+setupTests()
+describe('Landing Component', () => {
+    test('renders without crashing', () => {
+        render(
+            // <AuthContextProvider>
+                <Landing />
+            // </AuthContextProvider>
+        );
+        const titleElement = screen.getByText(/landing.hero.title/i);
+        expect(titleElement).toBeInTheDocument();
+    });
+});
