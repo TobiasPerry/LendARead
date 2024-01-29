3dc90143b48056e5bd152c1c5f292f1587c4eb7f
Marco4444 <mscilipoti@itba.edu.ar>
2024-01-29 19:35:51 -0300

    image is being correctly retrieved

--- frontend/src/components/userDetails/UserProfile.tsx
+++ frontend/src/components/userDetails/UserProfile.tsx
@@ -5,37 +5,38 @@ import {useTranslation} from "react-i18next";
 const UserProfile = ({ isCurrent }) => {
     const { t } = useTranslation();
     //asumo por ahora que es current
-    const {userDetails} = useContext(AuthContext);
+    const {userDetails, userImage} = useContext(AuthContext);
 
 
     return (
         <div className="user-profile-card" style={{
             display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center', // Align children center
-            justifyContent: 'center', // Center children vertically
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'center',
         }}>
             <div className="profile-image-container">
                 <img
-                    src={userDetails.image}
+                    src={userImage}
                     alt={`${userDetails.userName} profile`}
                     style={{
                         width: '100px',
-                        height: '100px',
                         borderRadius: '50%',
                         objectFit: 'cover',
                         border: '3px solid white',
                         boxShadow: '0 4px 8px rgba(0,0,0,0.1)',
+                        marginRight: '10px',
                     }}
                 />
             </div>
+            <div className="d-flex flex-column">
             <h3 className="mt-2">{userDetails.userName}</h3>
             {/* Display the rating and lender/borrower status */}
             <div>
                 <span>{t('borrower')} {userDetails.ratingAsBorrower} ★</span>
                 <span> - </span>
                 <span>{t('lender')} {userDetails.ratingAsLender} ★</span>
-            </div>
+            </div> </div>
         </div>
     );
 };
