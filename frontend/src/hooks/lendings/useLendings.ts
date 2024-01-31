6ee5a6210a463ee7c93db82924a101dfb5c21204
Marco4444 <mscilipoti@itba.edu.ar>
2024-01-31 13:06:08 -0300

    added logic of pagination to lendings

--- frontend/src/hooks/lendings/useLendings.ts
+++ frontend/src/hooks/lendings/useLendings.ts
@@ -5,24 +5,47 @@ import {extractId, LendingApi} from "../assetInstance/useUserAssetInstances.ts";
 // @ts-ignore
 import photoPlaceholder from "../../../public/static/profile_placeholder.jpeg";
 
+function parseLinkHeader(header) {
+    if (!header || header.length === 0) {
+        return {};
+    }
+
+    // Split parts by comma and parse each part into a named link
+    return header.split(',').reduce((links, part) => {
+        const section = part.split(';');
+        if (section.length !== 2) {
+            throw new Error("section could not be split on ';'");
+        }
+
+        const url = section[0].replace(/<(.*)>/, '$1').trim();
+        const name = section[1].replace(/rel="(.*)"/, '$1').trim();
+
+        links[name] = url;
+
+        return links;
+    }, {});
+}
+
 const useLendings = () => {
 
     const [lendings, setLendings] = useState([])
     const [totalPages, setTotalPages] = useState(0)
+    const [links, setLinks] = useState({ first: null, next: null, last: null, prev: null });
     const [currentPage, setCurrentPage] = useState(0)
     const PAGE_SIZE = 1
 
-    const getLendings = async (asset) => {
-        // if(asset === undefined || asset.assetinstance === undefined || asset.instanceid === undefined) return
+    const getLendings = async (asset, url = `/lendings?itemsPerPage=${PAGE_SIZE}`) => {
         if(asset === undefined || asset === null || asset.assetinstance === undefined) return
 
-        const params = {
+
+        const lendingsResponse = await api.get(url,{ params: {
             assetInstanceId: asset.assetinstanceid,
-            'itemsPerPage': PAGE_SIZE,
-        }
-        
-        const lendingsResponse = await api.get(`/lendings`,{ params: params } )
-        console.log(lendingsResponse.headers)
+        }})
+
+        //@ts-ignore
+        const linkHeader: any = lendingsResponse.headers.get("Link");
+        const parsedLinks = parseLinkHeader(linkHeader);
+        setLinks(parsedLinks);
         const lendings = lendingsResponse.data
 
         const mappedLendings = lendings.map(async (lending: LendingApi) => {
@@ -46,9 +69,21 @@ const useLendings = () => {
         setLendings(lendings_)
     }
 
-    const changePage = (page) => {
+    const changePage = async (page) => {
+        let url;
+        if (page === 0) {
+            url = links.first;
+        } else if (page > currentPage) {
+            url = links.next;
+        } else if (page < currentPage) {
+            url = links.prev;
+        }
 
-    }
+        if (url) {
+            await getLendings(url);
+            setCurrentPage(page);
+        }
+    };
 
     return {
         lendings, totalPages, currentPage, changePage, getLendings
