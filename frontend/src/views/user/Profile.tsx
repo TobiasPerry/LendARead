b744c2d75d341fb2df5a82fcbdebb90144f99d2b
Pedro <pdihax@gmail.com>
2024-02-01 18:32:51 -0300

    WIP

--- frontend/src/views/user/Profile.tsx
+++ frontend/src/views/user/Profile.tsx
@@ -19,8 +19,42 @@ const ProfileView = (props: any) => {
         "Borrower Reviews"
     ]
 
+    const [userNames, setUserNames] = useState([])
+
 
     const MOCK_profile_picture_src = '/static/user-placeholder.jpeg'
+    const MOCK_reviews = [
+        {
+            reviwer: 'http://example.com/user/1',
+            lending: "http://example.com/lending/1",
+            rating: 3,
+            review: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
+        },
+        {
+            reviwer: 'http://example.com/user/2',
+            lending: "http://example.com/lending/1",
+            rating: 2,
+            review: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
+        },
+        {
+            reviwer: 'http://example.com/user/1',
+            lending: "http://example.com/lending/1",
+            rating: 1,
+            review: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
+        },
+        {
+            reviwer: 'http://example.com/user/1',
+            lending: "http://example.com/lending/1",
+            rating: 4,
+            review: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
+        },
+        {
+            reviwer: 'http://example.com/user/2',
+            lending: "http://example.com/lending/1",
+            rating: 5,
+            review: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
+        }
+    ]
 
     const renderRating = (behavior: "LENDER" | "BORROWER", rating: number) => {
         if (behavior == "LENDER") {
@@ -28,27 +62,27 @@ const ProfileView = (props: any) => {
                 return <></>
             }
             return (
-            <>
-            {t('userProfile.lender')}
-            <span className="user-role-stars">
-                { rating <= 0 ? (
-                    "-.- " ) : (
-                    rating
-                )}
-                ★ </span>
-            </>
+                <>
+                    {t('userProfile.lender')}
+                    <span className="user-role-stars">
+                        {rating <= 0 ? (
+                            "-.- ") : (
+                            rating
+                        )}
+                        ★ </span>
+                </>
             )
         } else {
             return (
-            <>
-            {t('userProfile.borrower')}
-            <span className="user-role-stars">
-                { rating <= 0 ? (
-                    "-.- " ) : (
-                    rating
-                )}
-                ★</span>
-            </>
+                <>
+                    {t('userProfile.borrower')}
+                    <span className="user-role-stars">
+                        {rating <= 0 ? (
+                            "-.- ") : (
+                            rating
+                        )}
+                        ★</span>
+                </>
             )
         }
     }
@@ -64,22 +98,23 @@ const ProfileView = (props: any) => {
     const renderTabNav = () => {
         return (
             <ul className="nav nav-tabs" id="user-tab" role="tablist">
-            {tabs.map((tab, index) => {
-                return renderTabTitle(tab, index)
-            })}
+                {tabs.map((tab, index) => {
+                    return renderTabTitle(tab, index)
+                })}
             </ul>
         )
     }
 
-    const renderTabContent = () => {
-        const mock_review = {
-            reviwer: 'http://example.com/user/1',
-            lending: "http://example.com/lending/1",
-            rating: 3,
-            review: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
-        }
+    const renderTabContent = (reviews: any) => {
+        const reviews_with_usernames: any = []
+        reviews.forEach((review: any)  => {
+            reviews_with_usernames.push({
+                ...review,
+                reviewer: "John Doe"
+            })
+        })
         return (
-            <ProfileReviewCard review={mock_review}/>
+            <ProfileReviewCard review={mock_review} />
         )
     }
 
@@ -92,28 +127,28 @@ const ProfileView = (props: any) => {
                     <div className="position-relative">
                         <div className="user-profile-cell">
                             <img className="user-profile-picture" src={MOCK_profile_picture_src} alt="user profile picture" />
-                            { isCurrentUser ? (
+                            {isCurrentUser ? (
                                 <div className="user-change-picture-container" id="change-profile-pic-btn">
                                     <i className="fas fa-solid fa-camera"></i>
                                 </div>
-                                ) : null
+                            ) : null
                             }
                         </div>
                     </div>
                     <div className="user-info-profile">
-                        <h1>{profileDetails.userName}</h1> 
+                        <h1>{profileDetails.userName}</h1>
                         <p className="grey-text">
                             {renderRating("BORROWER", profileDetails.ratingAsBorrower)}
                             {renderRating("LENDER", profileDetails.ratingAsLender)}
                         </p>
                     </div>
-                    <hr/>
+                    <hr />
                     <div className="tabs-container">
                         {renderTabNav()}
                     </div>
-                    <div  className="tab-content" >
+                    <div className="tab-content" >
                         {
-                           renderTabContent()
+                            renderTabContent(MOCK_reviews)
                         }
                     </div>
                 </div>
