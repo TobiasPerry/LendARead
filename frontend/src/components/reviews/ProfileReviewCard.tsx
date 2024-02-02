a22f2e16a7a412351b5a53ad517694508fac858e
Marco4444 <mscilipoti@itba.edu.ar>
2024-02-02 20:12:40 -0300

    added modal to review card

--- frontend/src/components/reviews/ProfileReviewCard.tsx
+++ frontend/src/components/reviews/ProfileReviewCard.tsx
@@ -5,23 +5,22 @@ import {extractId} from "../../hooks/assetInstance/useUserAssetInstances.ts";
 import {Link} from "react-router-dom";
 import StarsReviews from "../viewAsset/StarsReviews.tsx";
 
-const ProfileReviewCard = ({review, setCurrentReview}) => {
-    const rating = review.rating;
+const ProfileReviewCard = ({review, clickedReview}) => {
 
     return (
-        <div className="row d-flex justify-content-center" style={{ width: '450px' }} onClick={() => setCurrentReview(review)}>
+        <div className="row d-flex justify-content-center" style={{ width: '450px' }} onClick={() => clickedReview(review)}>
             <div className="my-2">
                 <div className="card" style={{ borderRadius: '30px' }}>
                     <div className="card-body m-3">
                         <div className="row">
                             <div className="col-lg-4 justify-content-center align-items-center">
-                                <img src={`${review.reviewerDetails.image}`}
+                                <img src={review.reviewerDetails.image}
                                      className="rounded-circle img-fluid shadow-1" alt="avatar" width="50"
                                      height="50"/>
                                 <p className="fw-bold lead mb-2"><strong>{review.reviewerDetails.userName}</strong></p>
                             </div>
                             <div className="col-lg-8">
-                                <StarsReviews rating={rating}/>
+                                <StarsReviews rating={review.rating}/>
                                 <p className="fw-light mb-4 ellipsis-text">
                                     {review.review}
                                 </p>
--- frontend/src/components/reviews/UserReviews.tsx
+++ frontend/src/components/reviews/UserReviews.tsx
@@ -7,14 +7,29 @@ import {useState} from "react";
 const UserReviews = ({reviews,changePage, currentPage, totalPages}) => {
 
     const [showModal, setShowModal] = useState(false)
-    const [currentReview, setCurrentReview] = useState(null)
+    const [currentReview, setCurrentReview] = useState({
+        lending: "",
+        rating: 0,
+        review: "",
+        reviewer: "",
+        selfUrl:"",
+        image: "",
+        reviewerDetails: {
+            image: ""
+        }
+    })
+
+    const handleClickedReview = async (review) => {
+        await setCurrentReview(review)
+        setShowModal(true)
+    }
 
     return (
         <div>
         <div className="d-flex flex-row gap-3 mb-4">
             {(reviews.length > 0
                     ? reviews.map((review: ReviewApi, index) =>
-                        <ProfileReviewCard key={index} review={review} setCurrentReview={setCurrentReview}/>
+                        <ProfileReviewCard key={index} review={review} clickedReview={handleClickedReview}/>
                     )
                     : <p>No Lender reviews yet</p>
             )}
