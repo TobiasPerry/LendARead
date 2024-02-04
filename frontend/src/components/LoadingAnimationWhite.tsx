482a008b6e6f7e49b2398c11911c1f53652f7d1d
Marco4444 <mscilipoti@itba.edu.ar>
2024-02-04 16:07:27 -0300

    updated logout to reload page and thus all data stored in react internally

--- frontend/src/components/LoadingAnimationWhite.tsx
+++ frontend/src/components/LoadingAnimationWhite.tsx
@@ -10,17 +10,15 @@ const LoadingAnimationWhite = () => {
                 alignItems: 'center',
                 height: '100%', // Take full height of the parent
                 width: '100%', // Take full width of the parent
-                backgroundColor: 'transparent', // Set background color to white
                 margin: "auto",
                 top: 0,
                 left: 0,
                 marginTop: "50px"
             }}>
-                <div className="main-class" style={{
+                <div style={{
                     display: 'flex',
                     justifyContent: 'center',
                     alignItems: 'center',
-                    backgroundColor: 'white', // Set background color to white
                 }}>
                     <img style={{width: "200px" }} src={FaviconClaro} alt="Animated Image" className="fade-in-out"/>
                 </div>
