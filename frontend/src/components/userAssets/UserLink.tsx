6e16f78c4613e0069deb09ac452939bb8d27adc9
Marco4444 <mscilipoti@itba.edu.ar>
2024-02-04 15:08:06 -0300

    added more data to user link in my books

--- frontend/src/components/userAssets/UserLink.tsx
+++ frontend/src/components/userAssets/UserLink.tsx
@@ -2,11 +2,14 @@ import {Link} from "react-router-dom";
 import useUserDetails from "../../hooks/assetInstance/useUserDetails.ts";
 import {useEffect, useState} from "react";
 import {extractId} from "../../hooks/assetInstance/useUserAssetInstances.ts";
+import {useTranslation} from "react-i18next";
+import UserRating from "./UserRatingDiv.tsx";
 
 const UserLink = ({asset, state}) => {
 
     const [userId, setUserId] = useState("")
 
+    const {t} = useTranslation()
     useEffect(() => {
         if(asset !== undefined && asset.lending !== undefined) {
             const userId_ = state === "lended" ? extractId(asset.lending.borrowerUrl) : extractId(asset.lending.lenderUrl)
@@ -20,8 +23,11 @@ const UserLink = ({asset, state}) => {
 
     return (
         <Link to={`/user/${userId}`}>
+            <div>
             <img src={userDetails.image} className="rounded-circle" width="30" height="30" alt="logo"/>
             <span>{userDetails.userName}</span>
+              <UserRating userDetails={userDetails}/>
+            </div>
         </Link>
     );
 }
