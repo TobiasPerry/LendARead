e44ff7873516f3a186894c06a3f69a344fc2edc1
Marco4444 <mscilipoti@itba.edu.ar>
2024-02-05 09:06:19 -0300

    added minor stylying changes

--- frontend/src/components/userAssets/LendingsReviews.tsx
+++ frontend/src/components/userAssets/LendingsReviews.tsx
@@ -1,13 +1,14 @@
 import {useTranslation} from "react-i18next";
 import UserReviews from "../reviews/UserReviews.tsx";
 import useLendings from "../../hooks/lendings/useLendings.ts";
-import {useEffect} from "react";
+import  {useEffect} from "react";
 import useReviews from "../../hooks/reviews/useReviews.ts";
+import Snackbar from "../SnackBar.tsx";
 
 const LendingsReviews = ({asset}) => {
 
     const {t} = useTranslation();
-    const {lendingReviews, fetchLendingReviews} = useReviews();
+    const {lendingReviews, fetchLendingReviews, error} = useReviews();
 
     useEffect(()=> {
         if(asset !== undefined && asset.lending !== undefined)
@@ -22,6 +23,7 @@ const LendingsReviews = ({asset}) => {
                     <UserReviews totalPages={1} changePage={() => {}} currentPage={1} reviews={lendingReviews} />
                 </div>
         }
+            {error.status && <Snackbar message={error.text} />}
         </>
     )
 }
